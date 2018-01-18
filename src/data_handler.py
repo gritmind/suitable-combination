94d14f4d29f5228428156215467b8e85863a6346
Yeongsu Kim <kys5670i@naver.com>
2018-01-18 14:26:14 +0900

    Add files via upload

--- src/data_handler.py
+++ src/data_handler.py
@@ -16,7 +16,7 @@ ps = PorterStemmer()
 global allot_PRPR_VER
 global VOCA
 
-
+# modified
 
 ######################################################
 """ LOAD & PREPROCESSING & VECTORIZING DATASET """
